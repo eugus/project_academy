4297c4c652d1c0242d4a6fda5a96300bc0a3bf8a
Luis Gustavo <lgustavog27@gmail.com>
2024-02-05 15:25:29 -0300

    first commit

---
+++ src/main/java/br/com/versao2/Academia/AcademiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.versao2.Academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/versao2/Academia/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.versao2.Academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
